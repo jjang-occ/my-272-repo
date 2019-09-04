62bdefd808952dd50cf8336de9107072a67e3032
jjang12 <jjang12@O074139-InstD.coast.cccd.adx>
2019-09-04 12:35:50 -0700

    initial commit

---
+++ my-new-test-project/src/MyNewTestProgram.java
@@ -0,0 +1,12 @@
+
+public class MyNewTestProgram
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Hi");
+        System.out.println("There");
+    }
+
+}
