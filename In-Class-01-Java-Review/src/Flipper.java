085c24cb4c95546c3be62096a0b1d594ad1c5bc5
jjang12 <jjang12@O074139-InstD.coast.cccd.adx>
2019-09-04 13:12:33 -0700

    initial commit for In-Class-01-Java-Review

---
+++ In-Class-01-Java-Review/src/Flipper.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author jjang12
+ * @version 9/4/2019
+ */
+public class Flipper
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Coin Flipper program");
+
+    }
+
+}
