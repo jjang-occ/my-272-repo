027704f3c345a767c768124eb72c15fff990550b
justi <justi@JJ-XPS13-LAPTOP.home>
2019-09-11 00:40:25 -0700

    added h02 project

---
+++ CS272-h02/src/occ/cs272/h02c/LabeledPoint.java
@@ -0,0 +1,35 @@
+package occ.cs272.h02c;
+
+//TODO: implement toString
+
+public class LabeledPoint
+{
+    private double x;
+    private double y;
+    private String label;
+
+    /**
+     * Constructs a labeled point with a given position and label.
+     * @param anX the x-coordinate of the point
+     * @param aY the y-coordinate of the point
+     * @param aLabel the label of the point
+     */
+    public LabeledPoint(double anX, double aY, String aLabel)
+    {
+        x = anX;
+        y = aY;
+        label = aLabel;
+    }
+
+    // Implemented toString here...
+    public String toString()
+    {
+        return getClass().getName() + "[x=" + x + ",y=" + y + ",label=" + label + "]";
+    }
+
+    public static String check(double x, double y, String s)
+    {
+        LabeledPoint lp = new LabeledPoint(x, y, s);
+        return lp.toString();
+    }
+}
---
+++ CS272-h02/src/occ/cs272/h02c/LabeledPointTester.java
@@ -0,0 +1,10 @@
+package occ.cs272.h02c;
+
+public class LabeledPointTester {
+
+    public static void main(String[] args) {
+        LabeledPoint lp = new LabeledPoint(10, 20, "Hello");
+        System.out.println(lp);
+    }
+
+}
