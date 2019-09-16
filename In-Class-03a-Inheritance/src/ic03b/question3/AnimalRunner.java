be07b010f2cb7581a3fc897ed22c5666384b8ecd
jjang12 <jjang12@O074139-InstD.coast.cccd.adx>
2019-09-16 12:37:32 -0700

    made AnimalRunner, Dog, Cat classes

---
+++ In-Class-03a-Inheritance/src/ic03b/question3/AnimalRunner.java
@@ -0,0 +1,13 @@
+package ic03b.question3;
+
+import java.util.*;
+
+public class AnimalRunner
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Dog> dogcatList = new ArrayList<Dog>();
+        dogcatList.add(new Dog("Fred"));
+        // dogcatList.add(new Cat("Wanda"));
+    }
+}
